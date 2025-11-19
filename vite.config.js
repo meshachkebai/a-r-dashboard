1586a27746f771bca8f03d94dd1aa331a0db15d9
meshachkebai <meshachkebai@outlook.com>
2025-11-19 13:15:42 +1000

    Revert to static base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/a-r-dashboard/' : '/',
-}))
+  base: '/a-r-dashboard/',
+})
